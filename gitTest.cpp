2dbd6dbe34b649bebafe4d3859ca5f9fb0118cfc
RaedaEsaa <86717173+RaedaEsaa@users.noreply.github.com>
2023-03-26 11:21:38 +0300

    add first function (sum)

--- gitTest.cpp
+++ gitTest.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
 using namespace std;
+double sum(double x, double y);
 
 int main()
 {
-
-
+	double x , y;
+	cout<< "please enter x and y: \n";
+	cin >> x>>y;
+	cout<<"sum ="<< sum(x , y)<<endl;
+	
+	
 	system("pause");
 	return 0;
+}
+
+
+double sum(double x, double y)
+{
+	
+	return x+y;
+
 }
